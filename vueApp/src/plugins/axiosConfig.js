b9fee7a15b7b53a4b558f1185ebfa21d5b61763a
julio <jcrico27@misena.edu.co>
2020-12-27 17:42:07 -0500

    mysql services

--- vueApp/src/plugins/axiosConfig.js
+++ vueApp/src/plugins/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
+const URL_BASE = process.env.VUE_APP_URL_API || 'http://laravel-services/api'
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api'
+  // baseURL: 'http://127.0.0.1:8000/api'
+  baseURL: URL_BASE
 })
 export default axiosInstance
