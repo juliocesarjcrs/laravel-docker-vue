1c3eb813d9161935848e77ea0ea6075c44661cf0
julio <jcrico27@misena.edu.co>
2020-12-30 07:15:02 -0500

    version front ngnix

--- vueApp/src/plugins/axiosConfig.js
+++ vueApp/src/plugins/axiosConfig.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const URL_BASE = process.env.VUE_APP_URL_API || 'http://laravel-services/api'
+// const URL_BASE = process.env.VUE_APP_URL_API || 'http://laravel-services/api'
+const URL_BASE = 'http://laravel-services/api'
 const axiosInstance = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api'
   baseURL: URL_BASE
