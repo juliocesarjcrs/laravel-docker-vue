94f379b00c4fc71a4c13748acc1d7e605b6fc846
julio <jcrico27@misena.edu.co>
2020-12-30 20:03:54 -0500

    dist a ip fija

--- vueApp/src/plugins/axiosConfig.js
+++ vueApp/src/plugins/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 // const URL_BASE = process.env.VUE_APP_URL_API || 'http://laravel-services/api'
-const URL_BASE = 'http://laravel-services/api'
+const URL_BASE = 'http://10.100.144.175/api'
+// const URL_BASE = 'http://laravel-services/api'
+
 const axiosInstance = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api'
   baseURL: URL_BASE
